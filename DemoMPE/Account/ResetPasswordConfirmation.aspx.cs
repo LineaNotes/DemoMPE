8164f2091c3962012b9d4c571cb53a6b3cdcea99
LineaNotes <anze.premrl@hotmail.com>
2016-02-04 14:09:43 +0100

    Initialization - clean Webforms template with updated NuGet packages.

---
+++ DemoMPE/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace DemoMPE.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ DemoMPE/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DemoMPE.Startup))]
+namespace DemoMPE
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
